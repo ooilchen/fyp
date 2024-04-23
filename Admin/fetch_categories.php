26574ad2a5d4e6381444d4fe8147528fbbfd6715
ooilchen <125842284+ooilchen@users.noreply.github.com>
2024-04-23 15:01:44 +0800

    .

---
+++ Admin/fetch_categories.php
@@ -0,0 +1,15 @@
+<?php
+include "conn.php";
+
+$sql = "SELECT * FROM `category`";
+$result = $conn->query($sql);
+
+$data = array();
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        $data[] = $row;
+    }
+}
+
+echo json_encode($data);
+?>
